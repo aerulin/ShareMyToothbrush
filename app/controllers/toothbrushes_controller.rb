945caa1456d92fb2dd9b83379a3ad6c4fd81f986
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-11-22 16:25:25 +0100

    Seed, Index + Create ready

--- app/controllers/toothbrushes_controller.rb
+++ app/controllers/toothbrushes_controller.rb
@@ -7,12 +7,17 @@ class ToothbrushesController < ApplicationController
     @toothbrush = Toothbrush.new
   end
 
-  def save
+  def create
+    @toothbrush = Toothbrush.new(toothbrush_params)
+    # @user = User.find(params[:user_id])
+    # @toothbrush.user = @user
+    @toothbrush.save!
+    redirect_to toothbrushes_path
   end
 
   private
+
   def toothbrush_params
-
+    params.require(:toothbrush).permit(:title, :description, :condition, :price, :brand, :category_id, :user_id)
   end
-
 end
