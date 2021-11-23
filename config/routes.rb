3a0127b95dceb150155d23af5437040db6cfc681
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-11-23 14:33:52 +0100

    Included picture in show + links from TB Index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
-  resources :toothbrushes, only: [:new, :create, :index]
+  resources :toothbrushes, only: [:new, :create, :index, :show]
 end
