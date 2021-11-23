cdf1bd3a0b4f2807b8ed960b44fe341537b3c83e
Julie Queloz <juliequeloz@hotmail.com>
2021-11-23 15:11:18 +0100

    homepage part1

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
 
   def home
+    @toothbrushes_list = Toothbrush.all
+    @toothbrushes = Toothbrush.all.last(3).reverse
   end
 end
