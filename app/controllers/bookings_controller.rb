6231a29adfa28d1ac2015939fd2b86c9b04f1cb1
Aymeric Erulin <erulin.aymeric@gmail.com>
2021-11-23 16:09:05 +0100

    created bookings creation path & view

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,21 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+    @toothbrush = Toothbrush.find(params[:toothbrush_id])
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @toothbrush = Toothbrush.find(params[:toothbrush_id])
+    @booking.toothbrush = @toothbrush
+    @booking.save!
+    redirect_to toothbrush_path(@toothbrush)
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
