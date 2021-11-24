c32411662d7b07ccb8c00dba8e4f1bc14525c788
sonia jaunin <sonia_jaunin@hotmail.com>
2021-11-24 14:41:00 +0100

    register form fonctionnality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,13 @@ class PagesController < ApplicationController
   def home
     @toothbrushes_list = Toothbrush.all
     @toothbrushes = Toothbrush.all.last(3).reverse
+    @user_id = params[:id]
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id, :first_name, :last_name, :address, :email, :password, :photo_url, :photo)
+  end
+
 end
