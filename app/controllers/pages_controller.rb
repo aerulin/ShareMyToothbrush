348f1394fa40938c8990d97c85e303c41a842ff4
imanuel lombardo <imanuel.lombardo@hotmail.com>
2021-11-26 13:40:40 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,8 @@ class PagesController < ApplicationController
     @markers = @users_list.geocoded.map do |user|
       {
         lat: user.latitude,
-        lng: user.longitude
+        lng: user.longitude,
+        image_url: helpers.asset_url('https://res.cloudinary.com/dg2an4buq/image/upload/v1637929695/pngkit_tooth-brush-png_809504_vsi4jl.png')
       }
     end
 
